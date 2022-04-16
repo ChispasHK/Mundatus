bacecaa3128c0dea3b578f7063d84210c201b7d4
Ivan A*** <48454454+ChispasHK@users.noreply.github.com>
2022-04-16 16:21:25 +0200

    v1.2

--- www/upload.php
+++ www/upload.php
@@ -1,3 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Mundatus</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Custom stylesheet -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <!--Icons-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <!--fonts-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open%20Sans">
+    
+    <!-- favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="512x512" href="/favicon/android-chrome-512x512.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="/favicon/android-chrome-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+    <link rel="icon" type="image/vnd.microsoft.icon" href="/favicon/favicon.ico">
+
+  </head>
+  <body>
+    <nav class="navbar navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand mb-0 h1 Montserrat" href="/">Mundatus</a>
+        <a class="navbar-brand" href="https://github.com/ChispasHK/Mundatus"><i class="bi bi-github"></i> </a>
+      </div>
+    </nav>
+
 <?PHP
   if(!empty($_FILES['uploaded_file'])) {
    // $folder = shell_exec('pwd' '/file/');
@@ -146,12 +181,24 @@
 	$ext = pathinfo($filename, PATHINFO_EXTENSION);
 
 	if (!in_array($ext, $typeFile)) {
-		echo 'Error type file';
+		?>
+		<div class="alert alert-danger" role="alert">
+			<div class="text-center">
+				<i class="bi bi-exclamation-triangle"></i> <?php echo 'Error type file.'; ?> <a href="/" class="alert-link">Please try again</a>
+			</div>
+		</div>
+	<?php
 
 	} else {
 
 		if ( $_FILES['uploaded_file']['size'] > 512000000 /* size limiter must be changed by hand */ ) {
-		echo "Exceeded file size limit.";
+			?>
+				<div class="alert alert-danger" role="alert">
+					<div class="text-center">
+						<i class="bi bi-exclamation-triangle"></i> <?php echo "Exceeded file size limit."; ?> <a href="/" class="alert-link">Please try again</a>
+					</div>
+				</div>
+			<?php
 		} else {
 
 			if (move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path)) {
@@ -176,10 +223,26 @@
 			// remove all file in directory
 			shell_exec('rm -f '.$pathDir.'*');
 
-			} else {
-				echo "There was an error uploading the file, please try again!";
+			} else { 
+				?>
+					<div class="alert alert-danger" role="alert">
+						<div class="text-center">
+							<i class="bi bi-exclamation-triangle"></i> <?php echo "There was an error uploading the file, "; ?> <a href="/" class="alert-link">Please try again</a>
+						</div>
+					</div>
+				<?php
+				
 			}
 		}
 	}
 }
 ?>
+
+<footer class="footer mt-auto">
+      <div class="text-center">
+        Built and developed by  <a href="https://www.cybernotrum.com/"> Cyber Notrum </a>
+      </div>
+    </footer>
+
+  </body>
+</html>
