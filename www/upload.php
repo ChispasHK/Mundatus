b1b09f502f8c814401a70d39fb2ee4b8f75e6532
Ivan A*** <48454454+ChispasHK@users.noreply.github.com>
2022-04-30 13:23:11 +0200

    v1.2

--- www/upload.php
+++ www/upload.php
@@ -1,39 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>Mundatus</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <!-- Custom stylesheet -->
-    <link rel="stylesheet" href="css/style.css">
-    <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <!--Icons-->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
-    <!--fonts-->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open%20Sans">
-    
-    <!-- favicon -->
-    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="512x512" href="/favicon/android-chrome-512x512.png">
-    <link rel="icon" type="image/png" sizes="192x192" href="/favicon/android-chrome-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
-    <link rel="icon" type="image/vnd.microsoft.icon" href="/favicon/favicon.ico">
-
-  </head>
-  <body>
-    <nav class="navbar navbar-dark bg-dark">
-      <div class="container">
-        <a class="navbar-brand mb-0 h1 Montserrat" href="/">Mundatus</a>
-        <a class="navbar-brand" href="https://github.com/ChispasHK/Mundatus"><i class="bi bi-github"></i> </a>
-      </div>
-    </nav>
-
 <?PHP
+	// html code block with head and navbar
+	$html = '<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<title>Mundatus</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- CSS only -->
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+		<!-- Custom stylesheet -->
+		<link rel="stylesheet" href="css/style.css">
+		<!-- JavaScript Bundle with Popper -->
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+		<!--Icons-->
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+		<!--fonts-->
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open%20Sans">
+		
+		<!-- favicon -->
+		<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="512x512" href="/favicon/android-chrome-512x512.png">
+		<link rel="icon" type="image/png" sizes="192x192" href="/favicon/android-chrome-192x192.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+		<link rel="icon" type="image/vnd.microsoft.icon" href="/favicon/favicon.ico">
+
+	</head>
+	<body>
+		<nav class="navbar navbar-dark bg-dark">
+		<div class="container">
+			<a class="navbar-brand mb-0 h1 Montserrat" href="/">Mundatus</a>
+			<a class="navbar-brand" href="https://github.com/ChispasHK/Mundatus"><i class="bi bi-github"></i> </a>
+		</div>
+		</nav>'
+	;
+
   if(!empty($_FILES['uploaded_file'])) {
    // $folder = shell_exec('pwd' '/file/');
     $pathDir = getcwd() . "/file/";
@@ -181,6 +183,8 @@
 	$ext = pathinfo($filename, PATHINFO_EXTENSION);
 
 	if (!in_array($ext, $typeFile)) {
+		// variable with html code block
+		echo $html;
 		?>
 		<div class="alert alert-danger" role="alert">
 			<div class="text-center">
@@ -192,6 +196,8 @@
 	} else {
 
 		if ( $_FILES['uploaded_file']['size'] > 512000000 /* size limiter must be changed by hand */ ) {
+		// variable with html code block
+		echo $html;
 			?>
 				<div class="alert alert-danger" role="alert">
 					<div class="text-center">
@@ -224,6 +230,8 @@
 			shell_exec('rm -f '.$pathDir.'*');
 
 			} else { 
+				// variable with html code block
+				echo $html;
 				?>
 					<div class="alert alert-danger" role="alert">
 						<div class="text-center">
